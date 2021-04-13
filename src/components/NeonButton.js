082a7c3ad002bb7f361de2e1b81dfbbd606371bb
Wilhelm Hattenbach <wilhelm.hattenbach@yh.nackademin.se>
2021-04-13 20:43:39 +0200

    First commit

---
+++ src/components/NeonButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './CSS/style.css'
+
+function neonButton() {
+    return (
+        <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Neon Button
+        </a>
+    );
+}
+
+export default neonButton;
